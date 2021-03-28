cdb95700358bab8234e2a2b85dfa6d64cc1f28ca
Ben Elliott <benelliott111@gmail.com>
2021-03-28 12:14:08 -0400

    exercise model

---
+++ models/exercise.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  name: String,
+  type: String,
+  sets: Number,
+  reps: Number,
+  duration: Number,
+  distance: Number,
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
